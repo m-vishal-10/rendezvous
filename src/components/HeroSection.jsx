a4a56cc06796629ef01ed8a02fcd059ef0566b12
Vishal Bhalaje M <115567045+m-vishal-10@users.noreply.github.com>
2025-02-15 14:12:06 +0530

    "added Video"

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,12 +4,9 @@ const HeroSection = () => {
   return (
     <div style={styles.heroContainer}>
       <video autoPlay loop muted style={styles.heroVideo}>
-        <source src="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4" type="video/mp4" />
+        <source src="/public/intro.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      <div style={styles.heroContent}>
-        <h1 style={styles.glow}>Welcome to Rendezvous 2K25</h1>
-      </div>
     </div>
   );
 };
@@ -17,6 +14,7 @@ const HeroSection = () => {
 const styles = {
   heroContainer: {
     position: "relative",
+    top: -79,
     width: "100%",
     height: "100vh",
     overflow: "hidden",
@@ -28,19 +26,6 @@ const styles = {
     position: "absolute",
     top: 0,
     left: 0,
-  },
-  heroContent: {
-    position: "relative",
-    zIndex: 1,
-    textAlign: "center",
-    color: "white",
-    top: "50%",
-    transform: "translateY(-50%)",
-  },
-  glow: {
-    fontSize: "80px",
-    color: "white",
-    textShadow: "0 0 20px #ff4da6, 0 0 30px #e60073",
   },
 };
 
