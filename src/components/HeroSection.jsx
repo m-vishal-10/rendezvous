7cd970ba832e554358c1e0faaa9c70afa89f1887
Vishal Bhalaje M <115567045+m-vishal-10@users.noreply.github.com>
2025-02-16 22:30:10 +0530

    Changed background

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,22 +3,45 @@ import React from "react";
 const HeroSection = () => {
   return (
     <div style={styles.heroContainer}>
-      <video autoPlay loop muted style={styles.heroVideo}>
-        <source src="/public/13138099_1920_1080_30fps.mp4" type="video/mp4" />
-        Your browser does not support the video tag.
-      </video>
+        {/* <video autoPlay loop muted style={styles.heroVideo}>
+          <source src="/public/13138099_1920_1080_30fps.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video> */}
     </div>
   );
 };
 
 const styles = {
+  // heroContainer: {
+  //   position: "absolute",
+  //   top: 0,
+  //   zIndex: 1,
+  //   width: "100%",
+  //   height: "100%",
+  //   overflow: "hidden",
+  // },
+  // heroVideo: {
+  //   width: "100%",
+  //   height: "100%",
+  //   objectFit: "cover",
+  //   position: "absolute",
+  //   top: 0,
+  //   left: 0,
+  // },
   heroContainer: {
-    position: "absolute",
+    position: "fixed",
     top: 0,
-    zIndex: 1,
-    width: "100%",
+    zIndex: 0,
     height: "100%",
-    overflow: "hidden",
+    width: "100%",
+    display: "flex", // Assuming `items-center` refers to flex centering
+    alignItems: "center",
+    paddingLeft: "1.25rem", // px-5 (5 * 4px = 20px)
+    paddingRight: "1.25rem",
+    paddingTop: "6rem", // py-24 (24 * 4px = 96px)
+    paddingBottom: "6rem",
+    background:
+      "radial-gradient(125% 125% at 50% 10%, #000 40%, #63e 100%)",
   },
   heroVideo: {
     width: "100%",
