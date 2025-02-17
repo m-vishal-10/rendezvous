4559bc5dee9e597d0ed389b8c1cbedcf63fda465
manojkumar-13-official <manojkumaratarumugam@gmail.com>
2025-02-17 10:35:19 +0530

    Upating content in About Page

--- src/components/Schedule.jsx
+++ src/components/Schedule.jsx
@@ -15,28 +15,40 @@ const Schedule = () => {
         </thead>
         <tbody>
           <tr>
-            <td style={styles.cell}>ENVISION</td>
-            <td style={styles.cell}>John Doe</td>
-            <td style={styles.cell}>123-456-7890</td>
-            <td style={styles.cell}>CSE BLOCK- I AV HALL</td>
+            <td style={styles.cell}>Clash of Coders</td>
+            <td style={styles.cell}>--</td>
+            <td style={styles.cell}>--</td>
+            <td style={styles.cell}>--</td>
           </tr>
           <tr>
-            <td style={styles.cell}>CODE-RPG</td>
-            <td style={styles.cell}>Jane Smith</td>
-            <td style={styles.cell}>987-654-3210</td>
-            <td style={styles.cell}>COMPUTER CENTER - II</td>
+            <td style={styles.cell}>Paper Presentation</td>
+            <td style={styles.cell}>--</td>
+            <td style={styles.cell}>--</td>
+            <td style={styles.cell}>--</td>
           </tr>
           <tr>
-            <td style={styles.cell}>BGMI-TOURNAMENT</td>
-            <td style={styles.cell}>Michael Brown</td>
-            <td style={styles.cell}>555-678-1234</td>
-            <td style={styles.cell}>COMPUTER CENTER - III</td>
+            <td style={styles.cell}>Shark Tank</td>
+            <td style={styles.cell}>--</td>
+            <td style={styles.cell}>--</td>
+            <td style={styles.cell}>--</td>
           </tr>
           <tr>
-            <td style={styles.cell}>PEC's IPL AUCTION</td>
-            <td style={styles.cell}>Sarah Johnson</td>
-            <td style={styles.cell}>444-333-2222</td>
-            <td style={styles.cell}>CSE BLOCK- III AV HALL</td>
+            <td style={styles.cell}>Pixel Playground UI</td>
+            <td style={styles.cell}>--</td>
+            <td style={styles.cell}>--</td>
+            <td style={styles.cell}>--</td>
+          </tr>
+          <tr>
+            <td style={styles.cell}>IPL Auction</td>
+            <td style={styles.cell}>--</td>
+            <td style={styles.cell}>--</td>
+            <td style={styles.cell}>--</td>
+          </tr>
+          <tr>
+            <td style={styles.cell}>Clash of Clans</td>
+            <td style={styles.cell}>--</td>
+            <td style={styles.cell}>--</td>
+            <td style={styles.cell}>--</td>
           </tr>
         </tbody>
       </table>
