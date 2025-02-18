7d819237665c7269459865afa60d3c058050e6d9
FEROZ130405 <mohamedferoz778@gmail.com>
2025-02-18 11:23:36 +0530

    changed alignement and image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
       {/* Image Section (Replaced the Video with PNG Image) */}
       <div style={styles.imageContainer}>
         <img
-          src="/ren.png" // Replace with the path to your PNG image
+          src="/squid1.png" // Replace with the path to your PNG image
           alt="Background"
           style={styles.image}
         />
@@ -29,27 +29,13 @@ const Home = () => {
 const styles = {
   imageContainer: {
     position: "relative", 
-    zIndex: 10, // Higher than the background in Welcome
+    zIndex: 10,
     width: "100%",
-<<<<<<< HEAD
     height: "100%",
-=======
     height: "200vh",
->>>>>>> fa2dd544c7d1e078b64ab81995f691bbd4653c84
     display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "rgba(0, 0, 0, 0.7)", // Slight overlay for visibility
-<<<<<<< HEAD
-<<<<<<< HEAD
-    marginBottom: "300px",
-=======
-    marginBottom:"300px",
-    marginTop:"40px",
->>>>>>> 2ab000bfd48616bec94c3c56e1dddd225e7781d8
-=======
-    marginBottom:"30vh",
->>>>>>> fa2dd544c7d1e078b64ab81995f691bbd4653c84
   },
   image: {
     width: "100%", // Adjust width
